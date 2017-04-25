14ecc534d94e096a878e992314f9e3506c4df537
Alejandro Garcia <agarc579@fiu.edu>
2017-04-25 19:35:15 -0400

    properly naming the values in the company column

--- data_analysis.R
+++ data_analysis.R
@@ -1,3 +1,23 @@
 library(readxl)
+library(dplyr)
 refine <- read_excel("~/Downloads/refine.xlsx")
-head(refine)
+
+refine_df <- data.frame(refine)
+#toupper(refine_df$company)
+
+refine_df$company[0:6] <- "Phillips"
+refine_df$company[7:13] <- "Akzo"
+refine_df$company[14:16] <- "Phillips"
+refine_df$company[17:21] <- "Van Houten"
+refine_df$company[22:25] <- "Unilever"
+
+
+# 
+# for(x in refine_df$company) {
+#   if(startsWith(x, "a") = TRUE) {
+#     x <- "Akzo"
+#     print(refine_df$company[x])
+#   }
+# }
+
+
